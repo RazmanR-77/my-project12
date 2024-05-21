bdcdaca539176ed4c0a9f44c222ebdeae881d381
razman <razmanhraman@gmail.com>
2024-05-21 15:28:56 +0800

    showing weather info, colours and icons

--- weather-app1/src/WeatherApp.js
+++ weather-app1/src/WeatherApp.js
@@ -1,55 +1,147 @@
 // WeatherApp.js
+// author: by Razman Raman, developer  , date; 052024
 
-import React from 'react';
-import { useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect, useState } from 'react';
+import TemperatureWeatherDetail from './TemperatureWeatherDetail';
+import WeatherIconDetail from './WeatherIconDetail';
+import { weatherIcon } from './utils/IconsUtils';
+
+function WeatherApp() {
+  // author: by Razman Raman, developer  , date; 052024
+
+
+  // initialise the state
+  const [city, setCity] = useState("");
+  const [currentWeather, setCurrentWeather] = useState({
+    main: { temp: 0, humidity: 0 },
+    wind: { speed: 0 },
+    weather: [{ description: "" }]
+  });
+
  
 
-function WeatherApp() {  
+  useEffect(() => {
+    checkWeather();
+  }, []);
 
-    useEffect(() => {
-        create_request();
-      }, []);
-  
-let cityY;
-      // Awesome javascript way 
-      function create_request() {
-        let city = cityY;  
 
-        // fetch(api url )
-        let API_URL = "http://api.openweathermap.org/data/2.5/weather?q=" + city +
-            "&units=imperial&appid=e83b3c4c08285bf87b99f9bbc0abe3f0";
-        // $.get(API_URL, deliver); 
-        fetch(API_URL   )
+
+
+  function checkWeather() {
+    // request weather data
+
+    { console.log("city== ", city) }
+
+    // `${GEO_API_URL}`
+    try {
+      let API_URL = "http://api.openweathermap.org/data/2.5/weather?q=" + `${city}` +
+        "&units=celsius&appid=e83b3c4c08285bf87b99f9bbc0abe3f0";
+      fetch(API_URL)
         .then(res => res.json().then(data => {
-            console.log(data);
-            deliver(data);
-        
-        }) );
+          console.log(API_URL, data);
+          setCurrentWeather(data);
+
+          console.log("weather== ", currentWeather);
+
+
+        }));
+
+
+    } catch (error) {
+      console.log("error== ", error,
+        currentWeather);
 
     }
 
-    function deliver(response) {
-        let result = "<h4>" + response.name + "</h4>" +
-            "<ul>" +
-            "<li> temperature: " + response.main.temp + " F </li>" +
-            "<li> humidity: " + response.main.humidity + "% </li>" +
-            "<li> wind speed: " + response.wind.speed + " mph</li>" +
-            "</ul>";
-        // $(".result").html(result);
-    }
+  }
+
+
+
+  function HumidWindTempOtherDetails() {
+    return <>
+
+      <ul class="list-grp">
+        <li class="list-group-item active ">
+          temperature:      {Math.round(currentWeather.main.temp) / 10} °C </li>
+
+        <li class="list-group-item active "> humidity:   {currentWeather.main.humidity} % </li>
+        <li class="list-group-item active "> wind speed:  {currentWeather.wind.speed} km/h</li></ul>
+
+
+    </>
+  }
+
 
   return (
     <div>
-      <h1>Open Weather Map <a href="https://openweathermap.org/current">API</a></h1>
-      <h3>The Current Weather in</h3>
-      <div class="result"></div>
-      <input type="text" placeholder="City" value={cityY} />  
 
-    
-      <button onclick={(e) => create_request()}>Check Weather</button>
+      <h1>OpenWeatherMap.org
+        <span> <h3>   <a href="https://openweathermap.org/current" target="_blank">  <i class="fa fa-superscript" aria-hidden="true">API
+          reference</i>  </a> </h3> </span>
+      </h1>
+
+
+      <h3>Check The Current Weather in</h3>
+      <div>
+        {currentWeather.main && currentWeather
+          ? "Status: Ready" : "Status: Ready, to check the  weather. Need your input"}
+      </div>
+
+
+      <label>  </label>
+      <input type="text" placeholder=" Country/City  " value={city} onChange={(e) => setCity(e.target.value)} />
+
+      <button class="btn btn-primary d-auto" onClick={() => checkWeather()}  >Check Weather+</button>
+
+
+      <div class="result">
+        <div class="containerr">
+
+          <div class="row d-auto">
+            <div class="card  card-primary col-md-12">
+              <div class="cardw-body">
+                <h5 class="card-title card-primary col-md-8">{currentWeather.name}</h5>
+
+
+                {/* weather icon   */}
+                <div class="col-md-8">
+                  {currentWeather.weather != null ?
+                    <WeatherIconDetail src={weatherIcon(`${currentWeather.weather[0].icon}.png`)} /> : "No data"}
+
+                </div>
+                <div class="card-text col-md-8">
+
+                  <h4>
+                    <div>    {currentWeather.main != null ? <TemperatureWeatherDetail
+                      temperature={Math.round(currentWeather.main.temp / 10)}
+                      description={currentWeather.weather[0].description}
+                    /> : "No data"}
+                    </div>
+                  </h4>
+
+
+                </div>
+              </div>
+            </div>
+
+
+
+          </div>
+
+          <div>
+            {currentWeather.main != null ?
+              HumidWindTempOtherDetails() : "no data"}
+          </div>
+        </div>
+      </div>
+
     </div>
   );
+
+
 }
+
 
 export default WeatherApp;
 
